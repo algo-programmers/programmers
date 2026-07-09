0bf0ea6bbd4332f6d4dadbf8b3e95d1a579e51ec
hjw <hyejeong2823@naver.com>
2026-07-09 09:31:17 +0900

    .

---
+++ jeongwon/388351.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+        int len = schedules.length;
+        for(int i=0;i<len;i++){
+            int cnt = 0;
+            int timeLimit = ((schedules[i]+10)%100>=60) ? schedules[i]+50 : schedules[i]+10;
+            for(int j=0;j<7;j++){
+                if(((j+startday)%7>=1 && (j+startday)%7<=5) && timelogs[i][j]<=timeLimit){
+                    cnt++;
+                }
+            }
+            if(cnt==5)answer++;
+        }
+        return answer;
+    }
+}
+//
